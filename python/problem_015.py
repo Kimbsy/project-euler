651c0009a7f724feb7a8588d748aa73ec82e1aec
Kimbsy <dave.kimber@simitive.com>
2016-02-10 16:18:01 +0000

    waaaaaay too slow

--- python/problem_015.py
+++ python/problem_015.py
@@ -4,14 +4,38 @@ the right and down, there are exactly 6 routes to the bottom right corner.
 How many such routes are there through a 20×20 grid?
 """
 
-target = 2
+def check_option(option):
+    global total
+    if (option[0] == target) and (option[1] == target):
+        # print('total: ' + str(total))
+        return True
+    return False
 
-grid = []
+def get_options(pos):
+    options = []
+    row = pos[0]
+    column = pos[1]
+    if (row <= target):
+        options.append([row + 1, column])
+    if (column <= target):
+        options.append([row, column + 1])
 
-for i in range(0, target + 1):
-    row = []
-    for j in range(0, target + 1):
-        row.append(0)
-    grid.append(row)
+    return options
 
-print(grid)
+def get_options_recursive(pos):    
+    global total
+    options = get_options(pos)
+    for option in options:
+        if check_option(option):
+            total = total + 1
+        else: 
+            get_options_recursive(option)
+
+
+target = 20
+
+total = 0
+
+get_options_recursive([0, 0])
+
+print(total)
